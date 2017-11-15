1dfe1959c14c4b30c88cbce400087ee35413055d
dushide <2595839603@qq.com>
2017-11-16 01:47:06 +0800

    团队协作后端

---
+++ 团队协作后端/src/main/java/com/lfk/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lfk.repository;
+
+import com.lfk.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
