74fb9a5e90a9e9a8a12970a2f877430dd8c70756
dushide <2595839603@qq.com>
2017-11-15 23:22:40 +0800

    团队协作未完成

--- 团队协作/app/src/main/java/com/example/myapplication/AddProject_Activity.java
+++ 团队协作/app/src/main/java/com/example/myapplication/AddProject_Activity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.support.v4.app.SupportActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,16 +13,19 @@ import android.widget.ImageView;
 public  class AddProject_Activity extends AppCompatActivity implements View.OnClickListener {
     private EditText project_name;
     private EditText deadline;
+    private EditText item_describe;
     private Button back;
     private Button submit;
     String projectname;
     String deadlinetime;
+    String itemdescribe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_project_);
         project_name=(EditText)findViewById(R.id.project_name);
         deadline=(EditText)findViewById(R.id.deadline);
+        item_describe=(EditText)findViewById(R.id.item_describe);
         back = (Button) findViewById(R.id.back);
         submit = (Button)findViewById(R.id.submit);
          projectname=project_name.getText().toString();
@@ -29,6 +33,8 @@ public  class AddProject_Activity extends AppCompatActivity implements View.OnCl
         back.setOnClickListener(this);
         submit.setOnClickListener(this);
     }
+
+
     @Override
     public void onClick(View v){
         switch (v.getId()){
@@ -38,12 +44,16 @@ public  class AddProject_Activity extends AppCompatActivity implements View.OnCl
             case  R.id.submit:
                 projectname=project_name.getText().toString();
                 deadlinetime=deadline.getText().toString();
+                itemdescribe=item_describe.getText().toString();
                 Intent intent= new Intent();
-                intent.putExtra("data_return",projectname);
+                intent.putExtra("name",projectname);
+                intent.putExtra("deadline",deadlinetime);
+                intent.putExtra("describe",itemdescribe);
                 setResult(RESULT_OK,intent);
                 finish();
                 break;
             default:break;
         }
+
     }
 }
