622f5858ea4b0d0ba102f446297c62415dc8bc9f
hhhyg <1529014454@qq.com>
2017-12-12 18:07:32 +0800

    登录

--- 团队协作后端/src/main/java/com/lfk/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lfk.repository;
-
-import com.lfk.model.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-}
